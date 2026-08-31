59cae3010db26824d384663e55e465ba44fd4f16
quisiou <marco.casteleiro@gmail.com>
2026-08-31 19:07:31 +0200

    Added env var to suppress qml qpa services logging

--- home/quisiou/variables.nix
+++ home/quisiou/variables.nix
@@ -5,11 +5,12 @@
 
 {
     home.sessionVariables = {
-        XDG_CACHE_HOME = "${config.home.homeDirectory}/.cache";
-        STEAM_EXTRA_COMPAT_TOOLS_PATHS = "${config.home.homeDirectory}/.steam/root/compatibilitytools.d";
-        EDITOR = "nvim";
-        VISUAL = "nvim";
-        UV_PYTHON_PREFERENCE = "only-managed";
-        DICPATH = "${pkgs.hunspellDicts.en_US}/share/hunspell:${pkgs.hunspellDicts.es_ES}/share/hunspell";
+        XDG_CACHE_HOME                  =   "${config.home.homeDirectory}/.cache";
+        STEAM_EXTRA_COMPAT_TOOLS_PATHS  =   "${config.home.homeDirectory}/.steam/root/compatibilitytools.d";
+        UV_PYTHON_PREFERENCE            =   "only-managed";
+        QT_LOGGING_RULES                =   "qt.qpa.services.warning=false";
+        EDITOR  =   "nvim";
+        VISUAL  =   "nvim";
+        DICPATH =   "${pkgs.hunspellDicts.en_US}/share/hunspell:${pkgs.hunspellDicts.es_ES}/share/hunspell";
     };
 }
